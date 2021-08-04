54b23586058cd40e2a465f9299d4ef383cbd0310
Alx-Falila <85758478+Alx-Falila@users.noreply.github.com>
2021-08-04 16:02:53 +0000

    Change
    Change

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,4 @@
-#include <lists.h>
+#include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,17 +10,14 @@
  **/
 size_t print_listint(const listint_t *h)
 {
-/*temp is a listint_t type. It is a structure*/
-	const listint_t *temp = h;
-	size_t numNodes;
-/*
- * if temp is null, it means it is in the last node. For that reason the
- * condition is: while temp diferent from NULL
- */
-	for (numNodes = 0; temp != NULL; numNodes++)
+	size_t counter = 0;
+
+	while (h != NULL)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next; /*temp will point to the next node*/
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
 	}
-	return (numNodes);
+
+	return (counter);
 }
