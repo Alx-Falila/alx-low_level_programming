a68057f88cd7b0c666019e0ab9e46ef91eeca69e
Alx-Falila <85758478+Alx-Falila@users.noreply.github.com>
2021-08-05 13:42:02 +0000

    flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,24 @@
 #include "holberton.h"
-
 /**
- * flip_bits - Counts the number of bits needed to be
- *             flipped to get from one number to another.
- * @n: The number.
- * @m: The number to flip n to.
- *
- * Return: The necessary number of bits to flip to get from n to m.
- */
+* flip_bits - Function that returns the number of bits to convert in the second
+* number
+*@n: first number
+*@m: second number
+*Return: the number of bytes to flip
+*/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ m, bits = 0;
+	unsigned long int or;
+	unsigned int retvalue;
 
-	while (xor > 0)
+	or = n ^ m;
+
+	retvalue = 0;
+	while (or > 0)
 	{
-		bits += (xor & 1);
-		xor >>= 1;
+		if ((or & 1) != 0)
+			retvalue++;
+		or = or >> 1;
 	}
-
-	return (bits);
+	return (retvalue);
 }
